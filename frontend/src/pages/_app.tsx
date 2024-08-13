1557400ac9702dbb3c0596f5377d275c26a87c4a
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2024-08-13 14:01:13 +0200

    feat(amplify-auth): add authenticator component + trackit logo

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,22 +1,9 @@
 import { AppProps } from "next/app";
-
-import { Amplify } from "aws-amplify";
-import { Authenticator } from "@aws-amplify/ui-react";
-import '@aws-amplify/ui-react/styles.css';
-
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
-      userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID!,
-      identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID!,
-    },
-  },
-})
+import Authenticator from "@/components/Authenticator";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Authenticator hideSignUp={true}>
+    <Authenticator>
       {({ signOut, user }) => (
         <Component {...pageProps} signOut={signOut} user={user} />
       )}
