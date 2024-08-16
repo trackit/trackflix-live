40ef897e6c3f209066a10d09b4379b36721d1537
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2024-08-16 17:49:43 +0200

    feat(basic-components): add 404 page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,22 @@
+
+"use client";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const Custom404 = () => {
+  const router = useRouter();
+  const to = '/dashboard';
+  const delay = 0;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace(to);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return null;
+};
+
+export default Custom404;
