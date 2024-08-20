eecd701cccd306ade2f2bb566eae2e30fd701867
Mathieu Brujan <mathieu.brujan@epitech.eu>
2024-08-20 10:58:54 +0900

    feat(EncoderPresets): add AWS MediaPackage encoder presets

---
+++ backend/src/shared/encoder-presets/index.ts
@@ -0,0 +1,5 @@
+import live_event from './live_event.json'
+import live_action from './live_action.json'
+import http_streaming from './http_live_streaming.json'
+
+export { live_event, live_action, http_streaming }
