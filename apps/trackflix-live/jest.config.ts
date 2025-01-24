7a58c7a9e6dfd42eab9d1a17adb62b47969473f0
alexis-ld <alexisledinh@gmail.com>
2025-01-24 14:15:03 +0100

    Initial commit

---
+++ apps/trackflix-live/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'trackflix-live',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/trackflix-live',
+};
---
+++ apps/trackflix-live/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="trackflix-live" />
+    </div>
+  );
+}
+
+export default App;
