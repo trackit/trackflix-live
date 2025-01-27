ed81a7fbf6a4e564e58d0d0783417b9b2ce66c91
Axel Biehler <axel@trackit.io>
2025-01-27 11:25:52 +0100

    feat(authentication-webui): add authenticator to ui

---
+++ apps/webui/src/amplify.config.ts
@@ -0,0 +1,9 @@
+export const amplifyConfig = {
+  Auth: {
+    Cognito: {
+      region: import.meta.env.VITE_AWS_REGION!,
+      userPoolId: import.meta.env.VITE_USER_POOL_ID!,
+      userPoolClientId: import.meta.env.VITE_APP_CLIENT_ID!,
+    },
+  },
+};
