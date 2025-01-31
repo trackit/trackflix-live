92f307c78b7b03f4cab5e8d30dcd212a7c3e4274
Axel Biehler <56958957+axel-biehler@users.noreply.github.com>
2025-01-31 15:58:07 +0100

    feat(add-date-picker-single-asset-flow): add date time picker  (#16)
    * feat(time-date-picker): add component
    
    * feat(time-date-picker): fix eslint error
    
    * feat(time-date-picker): fix condition
    
    * feat(time-date-picker): improve ux
    
    * feat(date-time-picker): add color from daisy ui
    
    * feat(date-time-picker): remove useless style
    
    * feat(time-date-picker): add color and change time picker
    
    * feat(date-time-picker): add state management
    
    * feat(time-date-picker): remove useless code and make date required
    
    * feat(single-asset-flow): add time date picker to single asset flow
    
    * feat(single-asset-flow): change datetime picker to use input type=datetime-local
    
    * feat(single-asset-flow): remove useless dependency
    
    ---------
    
    Co-authored-by: Axel Biehler <axel@trackit.io>

--- apps/webui/src/app/app.tsx
+++ apps/webui/src/app/app.tsx
@@ -1,24 +1,7 @@
-import {SingleAssetFlow} from "@trackflix-live/single-asset-flow";
-import { Panel, ThemeSwitcher, TxTimeline, Clock, Timeline } from '@trackflix-live/ui';
-import { DateTime } from 'luxon';
+import { SingleAssetFlow } from '@trackflix-live/single-asset-flow';
+import { Panel, ThemeSwitcher } from '@trackflix-live/ui';
 
 export function App() {
-
-  const devSteps = [
-    { title: 'Pre-TX', datetime:  DateTime.now().toISO()},
-    { title: 'TX', datetime:  DateTime.now().plus({ seconds: 10 }).toISO()},
-    { title: 'Post-TX', datetime:  DateTime.now().plus({ seconds: 10}).plus({ minutes: 1 }).toISO()},
-    { title: 'End'},
-  ];
-
-  const devTimelineSteps = [
-    { text: 'Creating entity 1', completed: true },
-    { text: 'Creating entity 2', completed: true },
-    { text: 'Creating entity 3', completed: true },
-    { text: 'Creating entity 4', loading: true },
-    { text: 'Creating entity 5' },
-  ];
-
   return (
     <div className={'prose   '}>
       <div className={'absolute top-2 right-2'}>
@@ -31,30 +14,8 @@ export function App() {
         }
       >
         <Panel>
-          <div className={'relative'}>
-            <h1>Trackflix Live</h1>
-
-            <div className={'absolute right-0 top-0'}>
-
-              <Clock />
-            </div>
-          </div>
-          <hr />
-          <div className="px-10 py-4">
-            <TxTimeline steps={devSteps} />
-          </div>
-          <hr />
-          <div className={'flex'}>
-            <div className={'px-4'}>
-            <Timeline steps={devTimelineSteps} />
-            </div>
-            <div className={'flex-grow'}>
-
+          <h1>Trackflix Live</h1>
           <SingleAssetFlow />
-            </div>
-
-
-          </div>
         </Panel>
       </div>
     </div>
