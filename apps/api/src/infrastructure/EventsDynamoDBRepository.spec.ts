eec5004fbead68d30079da0e4ac8c98dab7019f5
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2025-02-03 10:24:16 +0000

    refactor(monorepo-api): use setup function to define variables in test

--- apps/api/src/infrastructure/EventsDynamoDBRepository.spec.ts
+++ apps/api/src/infrastructure/EventsDynamoDBRepository.spec.ts
@@ -2,62 +2,19 @@ import { EventsDynamoDBRepository } from './EventsDynamoDBRepository';
 import { Event, EventStatus } from '@trackflix-live/types';
 import { CreateTableCommand, DeleteTableCommand, DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
 
-const dynamoDBClient = new DynamoDBClient({
-  endpoint: 'http://localhost:8000',
-  credentials: {
-    accessKeyId: 'fakeAccessKeyId',
-    secretAccessKey: 'fakeSecretAccessKey',
-  },
-});
-
-const sampleEvent: Event = {
-  id: '988de49c-14c8-4926-a40a-2f70c6aebc8f',
-  name: 'Race car',
-  description: 'A race car event.',
-  onAirStartTime: new Date('2025-01-31T19:15:00+0000'),
-  onAirEndTime: new Date('2025-01-31T20:00:00+0000'),
-  source: {
-    bucket: 'sample-bucket',
-    key: 'sample-key',
-  },
-  status: EventStatus.CONFIRMED,
-};
-
-const createTable = async () => {
-  await dynamoDBClient.send(new CreateTableCommand({
-    TableName: 'EventsTable',
-    AttributeDefinitions: [
-      { AttributeName: 'id', AttributeType: 'S' },
-      { AttributeName: 'name', AttributeType: 'S' },
-    ],
-    KeySchema: [
-      { AttributeName: 'id', KeyType: 'HASH' },
-      { AttributeName: 'name', KeyType: 'RANGE' },
-    ],
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 1,
-      WriteCapacityUnits: 1,
-    },
-  }));
-}
-
-const deleteTable = async () => {
-  await dynamoDBClient.send(new DeleteTableCommand({
-    TableName: 'EventsTable',
-  }));
-}
-
-
 beforeAll(async () => {
+  const { createTable } = setup();
   await createTable();
 });
 
 afterAll(async () => {
+  const { deleteTable } = setup();
   await deleteTable();
 });
 
 describe('EventsDynamoDBRepository', () => {
   it('should create an event in DynamoDB', async () => {
+    const { dynamoDBClient, sampleEvent } = setup();
     const repository = new EventsDynamoDBRepository(dynamoDBClient, 'EventsTable');
 
     const response = await repository.createEvent(sampleEvent);
@@ -83,3 +40,57 @@ describe('EventsDynamoDBRepository', () => {
     });
   });
 });
+
+const setup = () => {
+  const dynamoDBClient = new DynamoDBClient({
+    endpoint: 'http://localhost:8000',
+    credentials: {
+      accessKeyId: 'fakeAccessKeyId',
+      secretAccessKey: 'fakeSecretAccessKey',
+    },
+  });
+
+  const sampleEvent: Event = {
+    id: '988de49c-14c8-4926-a40a-2f70c6aebc8f',
+    name: 'Race car',
+    description: 'A race car event.',
+    onAirStartTime: new Date('2025-01-31T19:15:00+0000'),
+    onAirEndTime: new Date('2025-01-31T20:00:00+0000'),
+    source: {
+      bucket: 'sample-bucket',
+      key: 'sample-key',
+    },
+    status: EventStatus.CONFIRMED,
+  };
+
+  const createTable = async () => {
+    await dynamoDBClient.send(new CreateTableCommand({
+      TableName: 'EventsTable',
+      AttributeDefinitions: [
+        { AttributeName: 'id', AttributeType: 'S' },
+        { AttributeName: 'name', AttributeType: 'S' },
+      ],
+      KeySchema: [
+        { AttributeName: 'id', KeyType: 'HASH' },
+        { AttributeName: 'name', KeyType: 'RANGE' },
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1,
+      },
+    }));
+  }
+
+  const deleteTable = async () => {
+    await dynamoDBClient.send(new DeleteTableCommand({
+      TableName: 'EventsTable',
+    }));
+  }
+
+  return {
+    dynamoDBClient,
+    sampleEvent,
+    createTable,
+    deleteTable,
+  }
+}
