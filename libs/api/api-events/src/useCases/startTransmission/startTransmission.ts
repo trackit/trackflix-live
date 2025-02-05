dba4f0e7833dacbcc82676467b806cecf18c5dd5
Alexandre Sauner <alexandre.snr@icloud.com>
2025-02-05 17:58:39 +0100

    feat(start-transmission-sfn): add start transmission state machine

--- libs/api/api-events/src/useCases/startTransmission/startTransmission.ts
+++ libs/api/api-events/src/useCases/startTransmission/startTransmission.ts
@@ -1,3 +1,5 @@
+import { SFNClient, StartExecutionCommand } from '@aws-sdk/client-sfn';
+
 export interface StartTransmissionUseCase {
   startTransmission(eventId: string): Promise<void>;
 }
@@ -5,5 +7,16 @@ export interface StartTransmissionUseCase {
 export class StartTransmissionUseCaseImpl implements StartTransmissionUseCase {
   public async startTransmission(eventId: string): Promise<void> {
     console.log(`Starting transmission ${eventId}`);
+
+    const sfn = new SFNClient();
+    await sfn.send(
+      new StartExecutionCommand({
+        stateMachineArn: process.env['START_TX_STATE_MACHINE'],
+        name: `TrackflixStartTx-${eventId}`,
+        input: JSON.stringify({
+          eventId,
+        }),
+      })
+    );
   }
 }
