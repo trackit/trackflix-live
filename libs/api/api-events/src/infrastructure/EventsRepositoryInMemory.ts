3791cf32cc7572e61a59b6f18a5ac45e60599d4a
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2025-02-07 11:15:53 +0000

    chore(api-list-events): remove log statement

--- libs/api/api-events/src/infrastructure/EventsRepositoryInMemory.ts
+++ libs/api/api-events/src/infrastructure/EventsRepositoryInMemory.ts
@@ -5,7 +5,6 @@ export class EventsRepositoryInMemory implements EventsRepository {
   public readonly events: Event[] = [];
 
   async createEvent(event: Event): Promise<void> {
-    console.log('Creating event', event);
     this.events.push(event);
   }
 
