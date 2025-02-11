da39145f3889521f240cf39b1ce34b7d045cf42a
Axel Biehler <56958957+axel-biehler@users.noreply.github.com>
2025-02-11 15:19:04 +0100

    feat(authentication-webui): add authenticator to ui (#19)
    * feat(authentication-webui): add authenticator to ui
    
    * fix: Update package-lock
    
    ---------
    
    Co-authored-by: Axel Biehler <axel@trackit.io>
    Co-authored-by: alexis-ld <alexisledinh@gmail.com>

--- apps/webui/src/app/app.tsx
+++ apps/webui/src/app/app.tsx
@@ -1,24 +1,32 @@
 import { SingleAssetFlow } from '@trackflix-live/single-asset-flow';
 import { Panel, ThemeSwitcher } from '@trackflix-live/ui';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import { amplifyConfig } from '../amplify.config';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(amplifyConfig);
 
 export function App() {
   return (
-    <div className={'prose   '}>
-      <div className={'absolute top-2 right-2'}>
-        <ThemeSwitcher />
-      </div>
+    <Authenticator hideSignUp>
+      <div className={'prose   '}>
+        <div className={'absolute top-2 right-2'}>
+          <ThemeSwitcher />
+        </div>
 
-      <div
-        className={
-          'flex justify-center items-center w-screen h-screen bg-base-200'
-        }
-      >
-        <Panel>
-          <h1>Trackflix Live</h1>
-          <SingleAssetFlow />
-        </Panel>
+        <div
+          className={
+            'flex justify-center items-center w-screen h-screen bg-base-200'
+          }
+        >
+          <Panel>
+            <h1>Trackflix Live</h1>
+            <SingleAssetFlow />
+          </Panel>
+        </div>
       </div>
-    </div>
+    </Authenticator>
   );
 }
 
