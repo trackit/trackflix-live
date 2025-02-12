734165f984699d6546f0532bfce88e47d3dc230d
alexis-ld <alexisledinh@gmail.com>
2025-02-12 14:39:20 +0100

    feat: Add API client and plug form

--- libs/webui/single-asset-flow/src/lib/single-asset-flow.tsx
+++ libs/webui/single-asset-flow/src/lib/single-asset-flow.tsx
@@ -1,13 +1,31 @@
 import { SingleAssetForm } from '@trackflix-live/forms';
+import { postEvent } from '@trackflix-live/api-client';
+import { useState } from 'react';
 
 export function SingleAssetFlow() {
-  const onSubmit = (data: { assetUrl: string }) => {
-    console.log(data);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = async (data: {
+    name: string;
+    description: string;
+    s3Bucket: string;
+    s3Key: string;
+    onAirStartTime: Date;
+    onAirEndTime: Date;
+  }) => {
+    try {
+      setIsSubmitting(true);
+      await postEvent(data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
     <div>
-      <SingleAssetForm onSubmit={onSubmit} />
+      <SingleAssetForm onSubmit={onSubmit} disabled={isSubmitting} />
     </div>
   );
 }
