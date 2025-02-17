f1a10a5acd345534b57d648600cea542009eef80
alexis-ld <alexisledinh@gmail.com>
2025-02-17 16:33:49 +0100

    feat: Fix post merge

--- libs/webui/status-view/src/lib/status-view.tsx
+++ libs/webui/status-view/src/lib/status-view.tsx
@@ -33,7 +33,7 @@ export function StatusView() {
         <div className={'p-8'}>
           <TxTimeline steps={devSteps} />
         </div>
-        <hr className={'mb-6'} />
+        <hr className={'m-6'} />
         <div className={'flex'}>
           <div className={'w-1/3'}>
             <Timeline steps={devTimelineSteps} />
