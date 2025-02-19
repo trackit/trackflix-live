c07128407d184b199e16193dce000617316937ae
Axel Biehler <axel@trackit.io>
2025-02-19 14:22:00 +0100

    feat(notistack): remove custom snackbar and styled-component

--- apps/webui/src/app/app.tsx
+++ apps/webui/src/app/app.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { MaterialDesignContent, SnackbarProvider } from 'notistack';
-import styled from 'styled-components';
 
 import { SingleAssetFlow } from '@trackflix-live/single-asset-flow';
 import { StatusView } from '@trackflix-live/status-view';
@@ -14,40 +13,10 @@ import { amplifyConfig } from '../amplify.config';
 
 Amplify.configure(amplifyConfig);
 
-const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => ({
-  '&.notistack-MuiContent-success': {
-    backgroundColor: '#DBEFDC',
-    color: '#004D03',
-    fontWeight: 400,
-    boxShadow: 'rgba(0,0,0, 0.2) 0px 4px 12px',
-    borderRadius: '8px',
-  },
-  '&.notistack-MuiContent-error': {
-    backgroundColor: '#FFDCDC',
-    color: '#8C0000',
-    fontWeight: 400,
-    boxShadow: 'rgba(0,0,0, 0.2) 0px 4px 12px',
-    borderRadius: '8px',
-  },
-  '&.notistack-MuiContent-warning': {
-    backgroundColor: '#FEF0CC',
-    color: '#8C5E00',
-    fontWeight: 400,
-    boxShadow: 'rgba(0,0,0, 0.2) 0px 4px 12px',
-    borderRadius: '8px',
-  },
-}));
-
 export function App() {
   return (
     <Authenticator hideSignUp>
-      <SnackbarProvider
-        Components={{
-          success: StyledMaterialDesignContent,
-          error: StyledMaterialDesignContent,
-          warning: StyledMaterialDesignContent,
-        }}
-      />
+      <SnackbarProvider />
       <div className="flex flex-col h-screen">
         <Topbar />
         <div
