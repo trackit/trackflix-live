b4b95341ea4cc130b24a9eba2527199cfef230e0
alexis-ld <alexisledinh@gmail.com>
2025-02-19 16:44:30 +0100

    feat: Update tests

--- libs/webui/status-view/src/lib/status-view.spec.tsx
+++ libs/webui/status-view/src/lib/status-view.spec.tsx
@@ -1,10 +1,20 @@
 import { render } from '@testing-library/react';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router';
 import StatusView from './status-view';
 
 describe('StatusView', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<StatusView />);
+    const queryClient = new QueryClient();
+
+    const { baseElement } = render(
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <StatusView />
+        </QueryClientProvider>
+      </BrowserRouter>
+    );
+
     expect(baseElement).toBeTruthy();
   });
 });
