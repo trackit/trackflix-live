dac748220f18a2646c8e97fe45ed0601a2e90c45
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2025-02-21 11:42:28 +0100

    feat(media-updates): add test cases for startLiveChannel

--- libs/api/api-events/src/useCases/startLiveChannel/startLiveChannel.test.ts
+++ libs/api/api-events/src/useCases/startLiveChannel/startLiveChannel.test.ts
@@ -5,7 +5,7 @@ import {
   TaskTokensRepositoryInMemory,
 } from '../../infrastructure';
 import { StartLiveChannelUseCaseImpl } from './startLiveChannel';
-import { EventMother, LogType } from '@trackflix-live/types';
+import { EventMother, EventUpdateAction, LogType } from '@trackflix-live/types';
 
 describe('Start live channel use case', () => {
   it('should start live channel', async () => {
@@ -62,11 +62,58 @@ describe('Start live channel use case', () => {
         taskToken,
       },
     ]);
+  });
+
+  it('should store logs', async () => {
+    const { useCase, eventsRepository } = setup();
+    const taskToken = 'sample_task_token';
+    const packageChannelId = '8354829';
+    const liveChannelArn =
+      'arn:aws:medialive:us-west-2:000000000000:channel:8626488';
+    const liveChannelId = '8626488';
+    const event = EventMother.basic().build();
+
+    await eventsRepository.createEvent(event);
+
+    await useCase.startLiveChannel({
+      eventId: event.id,
+      liveChannelId,
+      liveChannelArn,
+      packageChannelId,
+      taskToken,
+    });
 
     expect(eventsRepository.events[0].logs).toEqual([
       {
         timestamp: expect.any(Number),
         type: LogType.LIVE_CHANNEL_STARTED,
+      },
+    ]);
+  });
+
+  it('should emit event', async () => {
+    const { useCase, eventsRepository, eventUpdateSender } = setup();
+    const taskToken = 'sample_task_token';
+    const packageChannelId = '8354829';
+    const liveChannelArn =
+      'arn:aws:medialive:us-west-2:000000000000:channel:8626488';
+    const liveChannelId = '8626488';
+    const event = EventMother.basic().build();
+
+    await eventsRepository.createEvent(event);
+
+    await useCase.startLiveChannel({
+      eventId: event.id,
+      liveChannelId,
+      liveChannelArn,
+      packageChannelId,
+      taskToken,
+    });
+
+    expect(eventUpdateSender.eventUpdates).toEqual([
+      {
+        action: EventUpdateAction.EVENT_UPDATE_UPDATE,
+        value: eventsRepository.events[0],
       },
     ]);
   });
@@ -90,5 +137,6 @@ const setup = () => {
     liveChannelsManager,
     useCase,
     eventsRepository,
+    eventUpdateSender,
   };
 };
