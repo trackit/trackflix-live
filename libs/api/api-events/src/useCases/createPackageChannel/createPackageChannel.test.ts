a8c00ce7d6d8a74be1fb338db3e23dcba353910b
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2025-02-21 11:30:40 +0100

    refactor(media-updates): split test into multiple test cases

--- libs/api/api-events/src/useCases/createPackageChannel/createPackageChannel.test.ts
+++ libs/api/api-events/src/useCases/createPackageChannel/createPackageChannel.test.ts
@@ -51,6 +51,72 @@ describe('Create Package channel use case', () => {
       },
     ]);
   });
+
+  it('should store logs channel', async () => {
+    const { useCase, packageChannelsManager, eventsRepository } = setup();
+    const packageChannelId = '8123456';
+
+    const event = EventMother.basic().withEndpoints([]).build();
+    await eventsRepository.createEvent(event);
+
+    packageChannelsManager.setPackageChannelId(packageChannelId);
+
+    await useCase.createPackageChannel(event.id);
+
+    expect(eventsRepository.events[0].logs).toEqual([
+      {
+        timestamp: expect.any(Number),
+        type: LogType.PACKAGE_CHANNEL_CREATED,
+      },
+    ]);
+  });
+
+  it('should store endpoints', async () => {
+    const { useCase, packageChannelsManager, eventsRepository } = setup();
+    const packageChannelId = '8123456';
+
+    const event = EventMother.basic().withEndpoints([]).build();
+    await eventsRepository.createEvent(event);
+
+    packageChannelsManager.setPackageChannelId(packageChannelId);
+
+    await useCase.createPackageChannel(event.id);
+
+    expect(eventsRepository.events[0].endpoints).toEqual([
+      {
+        url: `https://trackflix-live.mediapackage.com/${event.id}/index.m3u8`,
+        type: EndpointType.HLS,
+      },
+      {
+        url: `https://trackflix-live.mediapackage.com/${event.id}/index.mpd`,
+        type: EndpointType.DASH,
+      },
+    ]);
+  });
+
+  it('should emit events', async () => {
+    const {
+      useCase,
+      packageChannelsManager,
+      eventsRepository,
+      eventUpdateSender,
+    } = setup();
+    const packageChannelId = '8123456';
+
+    const event = EventMother.basic().withEndpoints([]).build();
+    await eventsRepository.createEvent(event);
+
+    packageChannelsManager.setPackageChannelId(packageChannelId);
+
+    await useCase.createPackageChannel(event.id);
+
+    expect(eventUpdateSender.eventUpdates).toEqual([
+      {
+        action: EventUpdateAction.EVENT_UPDATE_UPDATE,
+        value: eventsRepository.events[0],
+      },
+    ]);
+  });
 });
 
 const setup = () => {
