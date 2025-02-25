c691eaa8a3a24eaa17abffe2030f4671e35058a1
Alexandre Sauner <alexandre.snr@icloud.com>
2025-02-25 15:29:22 +0100

    feat(stop-db-updates): add LIVE_CHANNEL_STOPPED log

--- libs/api/api-events/src/useCases/deleteLiveChannel/deleteLiveChannel.test.ts
+++ libs/api/api-events/src/useCases/deleteLiveChannel/deleteLiveChannel.test.ts
@@ -1,10 +1,11 @@
 import {
   EventsRepositoryInMemory,
+  EventUpdateSenderFake,
   LiveChannelsManagerFake,
   TaskTokensRepositoryInMemory,
 } from '../../infrastructure';
 import { DeleteLiveChannelUseCaseImpl } from './deleteLiveChannel';
-import { EventMother } from '@trackflix-live/types';
+import { EventMother, EventUpdateAction, LogType } from '@trackflix-live/types';
 
 describe('Delete live channel use case', () => {
   it('should delete live channel', async () => {
@@ -112,23 +113,78 @@ describe('Delete live channel use case', () => {
       })
     ).rejects.toThrow('Missing live channel ID or live channel ARN.');
   });
+
+  it('should append live channel stopped log', async () => {
+    const { eventsRepository, useCase } = setup();
+    const eventId = '51b09cc5-4d24-452c-9198-216a2a06dd6d';
+    const liveChannelArn =
+      'arn:aws:medialive:us-west-2:000000000000:channel:8626488';
+    const liveChannelId = '8626488';
+    const event = EventMother.basic()
+      .withId(eventId)
+      .withLiveChannelArn(liveChannelArn)
+      .withLiveChannelId(liveChannelId)
+      .build();
+    await eventsRepository.createEvent(event);
+
+    await useCase.deleteLiveChannel({
+      eventId,
+      taskToken: 'sample_task_token',
+    });
+
+    expect(eventsRepository.events[0].logs).toEqual([
+      {
+        timestamp: expect.any(Number),
+        type: LogType.LIVE_CHANNEL_STOPPED,
+      },
+    ]);
+  });
+
+  it('should emit live update', async () => {
+    const { eventUpdateSender, eventsRepository, useCase } = setup();
+    const eventId = '51b09cc5-4d24-452c-9198-216a2a06dd6d';
+    const liveChannelArn =
+      'arn:aws:medialive:us-west-2:000000000000:channel:8626488';
+    const liveChannelId = '8626488';
+    const event = EventMother.basic()
+      .withId(eventId)
+      .withLiveChannelArn(liveChannelArn)
+      .withLiveChannelId(liveChannelId)
+      .build();
+    await eventsRepository.createEvent(event);
+
+    await useCase.deleteLiveChannel({
+      eventId,
+      taskToken: 'sample_task_token',
+    });
+
+    expect(eventUpdateSender.eventUpdates).toEqual([
+      {
+        action: EventUpdateAction.EVENT_UPDATE_UPDATE,
+        value: eventsRepository.events[0],
+      },
+    ]);
+  });
 });
 
 const setup = () => {
   const liveChannelsManager = new LiveChannelsManagerFake();
   const taskTokensRepository = new TaskTokensRepositoryInMemory();
   const eventsRepository = new EventsRepositoryInMemory();
+  const eventUpdateSender = new EventUpdateSenderFake();
 
   const useCase = new DeleteLiveChannelUseCaseImpl({
     liveChannelsManager,
     taskTokensRepository,
     eventsRepository,
+    eventUpdateSender,
   });
 
   return {
     liveChannelsManager,
     taskTokensRepository,
     eventsRepository,
+    eventUpdateSender,
     useCase,
   };
 };
