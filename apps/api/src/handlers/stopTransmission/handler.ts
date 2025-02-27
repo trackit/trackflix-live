dbb9bef7355d7c12ee89980b8212d5c334c3827f
Alexandre Sauner <alexandre.snr@icloud.com>
2025-02-27 18:01:32 +0100

    chore(di): fix typecheck

--- apps/api/src/handlers/stopTransmission/handler.ts
+++ apps/api/src/handlers/stopTransmission/handler.ts
@@ -1,14 +1,9 @@
-import { StopTransmissionUseCaseImpl } from '@trackflix-live/api-events';
 import { StopTransmissionAdapter } from './stopTransmission.adapter';
 import { registerProductionInfrastructure } from '../../infrastructure/registerProductionInfrastructure';
 
 registerProductionInfrastructure();
 
-const useCase = new StopTransmissionUseCaseImpl();
-
-const adapter = new StopTransmissionAdapter({
-  useCase,
-});
+const adapter = new StopTransmissionAdapter();
 
 export const main = async (event: { eventId: string }): Promise<void> => {
   await adapter.handle(event);
