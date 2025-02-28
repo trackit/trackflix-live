b5ca1af2b4525d70683bfa33f0c3aa41c29b2db5
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2025-02-28 16:38:38 +0000

    refactor(one-time-schedule): expect any string as input

--- apps/api/src/infrastructure/EventBridgeScheduler.spec.ts
+++ apps/api/src/infrastructure/EventBridgeScheduler.spec.ts
@@ -29,7 +29,7 @@ describe('EventBridgeScheduler', () => {
       ScheduleExpression: 'at(2022-01-01T00:00:00)',
       ActionAfterCompletion: 'DELETE',
       Target: {
-        Input: JSON.stringify({ eventId }),
+        Input: expect.any(String),
         Arn: target,
         RoleArn: roleArn,
       },
