5c631b7873792470c97b77a4d86f7f757b7e42e7
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2025-02-28 16:15:20 +0000

    feat(api-delete-event): add conditions to delete event

--- libs/api/api-events/src/useCases/deleteEvent/deleteEvent.test.ts
+++ libs/api/api-events/src/useCases/deleteEvent/deleteEvent.test.ts
@@ -1,6 +1,6 @@
 import { DeleteEventUseCaseImpl } from './deleteEvent';
 import { EventsRepositoryInMemory } from '../../infrastructure/EventsRepositoryInMemory';
-import { EventMother } from '@trackflix-live/types';
+import { EventMother, EventStatus } from '@trackflix-live/types';
 
 describe('DeleteEvent use case', () => {
   it('should delete event', async () => {
@@ -19,6 +19,35 @@ describe('DeleteEvent use case', () => {
 
     await expect(useCase.deleteEvent('non-existing-id')).rejects.toThrow(
       'Event not found'
+    );
+  });
+
+  it('should throw an error if the event status is not PRE_TX', async () => {
+    const { eventsRepository, useCase } = setup();
+
+    const event = EventMother.basic().withStatus(EventStatus.POST_TX).build();
+    await eventsRepository.createEvent(event);
+
+    await expect(useCase.deleteEvent(event.id)).rejects.toThrow(
+      'Event cannot be deleted'
+    );
+  });
+
+  it('should throw an error if the current time is between onAirStartTime and onAirEndTime', async () => {
+    const { eventsRepository, useCase } = setup();
+
+    const event = EventMother.basic()
+      .withOnAirStartTime('2025-01-01T00:00:00Z')
+      .withOnAirEndTime('2025-01-01T01:00:00Z')
+      .build();
+    await eventsRepository.createEvent(event);
+
+    jest
+      .spyOn(global.Date, 'now')
+      .mockImplementation(() => new Date('2025-01-01T00:30:00Z').getTime());
+
+    await expect(useCase.deleteEvent(event.id)).rejects.toThrow(
+      'Cannot delete event while it is on air'
     );
   });
 });
--- libs/api/api-events/src/useCases/deleteEvent/deleteEvent.ts
+++ libs/api/api-events/src/useCases/deleteEvent/deleteEvent.ts
@@ -1,4 +1,5 @@
 import { EventsRepository } from '../../ports';
+import { EventStatus } from '@trackflix-live/types';
 
 export interface DeleteEventUseCase {
   deleteEvent(eventId: string): Promise<void>;
@@ -16,6 +17,25 @@ export class DeleteEventUseCaseImpl implements DeleteEventUseCase {
   }
 
   public async deleteEvent(eventId: string): Promise<void> {
+    const event = await this.eventsRepository.getEvent(eventId);
+
+    if (!event) {
+      throw new Error('Event not found');
+    }
+    if (event.status !== EventStatus.PRE_TX) {
+      throw new Error('Event cannot be deleted');
+    }
+
+    const currentDate = Date.now();
+    const onAirStartTime = new Date(event.onAirStartTime);
+    const onAirEndTime = new Date(event.onAirEndTime);
+    if (
+      currentDate >= onAirStartTime.getTime() &&
+      currentDate <= onAirEndTime.getTime()
+    ) {
+      throw new Error('Cannot delete event while it is on air');
+    }
+
     await this.eventsRepository.deleteEvent(eventId);
   }
 }
