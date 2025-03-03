944ca3f7df92f4ba9fccc4b07072bd3e25c08f80
Alexandre Sauner <alexandre.snr@icloud.com>
2025-03-03 15:27:09 +0100

    feat(5-min-pre-tx): set pre-tx to 5 minutes before on air (#55)

--- libs/api/api-events/src/useCases/createEvent/createEvent.ts
+++ libs/api/api-events/src/useCases/createEvent/createEvent.ts
@@ -41,7 +41,7 @@ export class CreateEventUseCaseImpl implements CreateEventUseCase {
     await this.eventsRepository.createEvent(event);
 
     const preTxTime = new Date(event.onAirStartTime);
-    preTxTime.setMinutes(preTxTime.getMinutes() - 15);
+    preTxTime.setMinutes(preTxTime.getMinutes() - 5);
 
     await this.eventSchedulerStart.scheduleEvent({
       id,
