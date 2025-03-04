c69c8e908607aac8e197b10f449218965a071d8c
Alexandre Sauner <alexandre.snr@icloud.com>
2025-03-04 18:10:18 +0100

    feat(api-delete-event): refactor event scheduler tests

--- apps/api/src/infrastructure/EventBridgeScheduler.spec.ts
+++ apps/api/src/infrastructure/EventBridgeScheduler.spec.ts
@@ -13,82 +13,88 @@ describe('EventBridgeScheduler', () => {
     mock.reset();
   });
 
-  it('should schedule an event', async () => {
-    const { scheduler, target, roleArn } = setup();
-    const eventId = '005fc3b5-623c-41f6-8885-22dc72f30676';
+  describe('scheduleEvent', () => {
+    it('should schedule an event', async () => {
+      const { scheduler, target, roleArn } = setup();
+      const eventId = '005fc3b5-623c-41f6-8885-22dc72f30676';
 
-    await scheduler.scheduleEvent({
-      name: 'TrackflixLiveTx',
-      id: eventId,
-      time: new Date('2022-01-01T00:00:00Z'),
-    });
-
-    const createScheduleCommandCalls = mock.commandCalls(CreateScheduleCommand);
-    expect(createScheduleCommandCalls).toHaveLength(1);
-    expect(createScheduleCommandCalls[0].args[0].input).toEqual({
-      Name: `TrackflixLiveTx-${eventId}`,
-      ScheduleExpression: 'at(2022-01-01T00:00:00)',
-      ActionAfterCompletion: 'DELETE',
-      Target: {
-        Input: expect.any(String),
-        Arn: target,
-        RoleArn: roleArn,
-      },
-      FlexibleTimeWindow: {
-        Mode: 'OFF',
-      },
-    });
-    expect(
-      JSON.parse(
-        createScheduleCommandCalls[0].args[0].input.Target?.Input || '{}'
-      )
-    ).toEqual({
-      eventId,
-    });
-  });
-
-  it('should throw an error when scheduling an event fails', async () => {
-    const { scheduler } = setup();
-    mock
-      .on(CreateScheduleCommand)
-      .rejects(new Error('Failed to schedule event'));
-
-    await expect(
-      scheduler.scheduleEvent({
-        name: 'test-event',
-        id: 'my-scheduled-event',
+      await scheduler.scheduleEvent({
+        name: 'TrackflixLiveTx',
+        id: eventId,
         time: new Date('2022-01-01T00:00:00Z'),
-      })
-    ).rejects.toThrow('Failed to schedule event');
+      });
+
+      const createScheduleCommandCalls = mock.commandCalls(
+        CreateScheduleCommand
+      );
+      expect(createScheduleCommandCalls).toHaveLength(1);
+      expect(createScheduleCommandCalls[0].args[0].input).toEqual({
+        Name: `TrackflixLiveTx-${eventId}`,
+        ScheduleExpression: 'at(2022-01-01T00:00:00)',
+        ActionAfterCompletion: 'DELETE',
+        Target: {
+          Input: expect.any(String),
+          Arn: target,
+          RoleArn: roleArn,
+        },
+        FlexibleTimeWindow: {
+          Mode: 'OFF',
+        },
+      });
+      expect(
+        JSON.parse(
+          createScheduleCommandCalls[0].args[0].input.Target?.Input || '{}'
+        )
+      ).toEqual({
+        eventId,
+      });
+    });
+
+    it('should throw an error when scheduling an event fails', async () => {
+      const { scheduler } = setup();
+      mock
+        .on(CreateScheduleCommand)
+        .rejects(new Error('Failed to schedule event'));
+
+      await expect(
+        scheduler.scheduleEvent({
+          name: 'test-event',
+          id: 'my-scheduled-event',
+          time: new Date('2022-01-01T00:00:00Z'),
+        })
+      ).rejects.toThrow('Failed to schedule event');
+    });
+
+    it('should throw an error if an invalid Date is passed', async () => {
+      const { scheduler } = setup();
+
+      await expect(
+        scheduler.scheduleEvent({
+          name: 'test-event',
+          id: 'my-scheduled-event',
+          time: new Date('Invalid date'),
+        })
+      ).rejects.toThrow('Invalid date');
+    });
   });
 
-  it('should throw an error if an invalid Date is passed', async () => {
-    const { scheduler } = setup();
+  describe('deleteSchedule', () => {
+    it('should delete schedule', async () => {
+      const { scheduler } = setup();
 
-    await expect(
-      scheduler.scheduleEvent({
-        name: 'test-event',
-        id: 'my-scheduled-event',
-        time: new Date('Invalid date'),
-      })
-    ).rejects.toThrow('Invalid date');
-  });
+      await scheduler.deleteSchedule('test-event');
+    });
 
-  it('should delete schedule', async () => {
-    const { scheduler } = setup();
+    it('should throw an error if the delete schedule throws an error', async () => {
+      const { scheduler } = setup();
+      mock
+        .on(DeleteScheduleCommand)
+        .rejects(new Error('Failed to delete schedule'));
 
-    await expect(scheduler.deleteSchedule('test-event')).resolves.not.toThrow();
-  });
-
-  it('should throw an error if the delete schedule throws an error', async () => {
-    const { scheduler } = setup();
-    mock
-      .on(DeleteScheduleCommand)
-      .rejects(new Error('Failed to delete schedule'));
-
-    await expect(scheduler.deleteSchedule('test-event')).rejects.toThrow(
-      'Failed to delete schedule'
-    );
+      await expect(scheduler.deleteSchedule('test-event')).rejects.toThrow(
+        'Failed to delete schedule'
+      );
+    });
   });
 });
 
